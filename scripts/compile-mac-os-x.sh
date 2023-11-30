5da3d8d0ba4442ce9fa7258f733c49e5f8f6dca3
Matvey Gladkikh <matvey@copters.ru>
2023-11-30 22:45:36 +0300

    crosscompilation mac

--- scripts/compile-mac-os-x.sh
+++ scripts/compile-mac-os-x.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-GOOS=darwin GOARCH=amd64 go build -ldflags="-w -s" -ldflags="mmacosx-version-min=10.9" -o macos-web-app-10.9.app/Contents/MacOS/Restar.amd64
-GOOS=darwin GOARCH=arm64 CGO_ENABLED=1 go build -ldflags="-w -s" -ldflags="mmacosx-version-min=10.9" -o macos-web-app-10.9.app/Contents/MacOS/Restar.arm64
+GOOS=darwin GOARCH=amd64 go build -ldflags "-w -s -linkmode=external -extldflags '-mmacosx-version-min=10.9'" -o macos-web-app-10.9.app/Contents/MacOS/Restar.amd64
+GOOS=darwin GOARCH=arm64 CGO_ENABLED=1 go build -ldflags "-w -s -linkmode=external -extldflags '-mmacosx-version-min=10.9'" -o macos-web-app-10.9.app/Contents/MacOS/Restar.arm64
 lipo -create -output "macos-web-app-10.9.app/Contents/MacOS/Restar" "macos-web-app-10.9.app/Contents/MacOS/Restar.amd64" "macos-web-app-10.9.app/Contents/MacOS/Restar.arm64"
