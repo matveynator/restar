463eafb0bf38e7f3aa3feacd7ec9f5290e9bc3b2
Matvey Gladkikh <matvey@copters.ru>
2023-11-30 22:09:34 +0300

    amd64

--- scripts/crosscompile.sh
+++ scripts/crosscompile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-set -x
-GOOS=darwin GOARCH=amd64 go build -ldflags="-w -s" -ldflags="mmacosx-version-min=10.9" -o macos-web-app-10.9.app/Contents/MacOS/Restar
+GOOS=darwin GOARCH=amd64 go build -ldflags="-w -s" -ldflags="mmacosx-version-min=10.9" -o macos-web-app-10.9.app/Contents/MacOS/Restar.amd64
+GOOS=darwin GOARCH=arm64 go build -ldflags="-w -s" -ldflags="mmacosx-version-min=10.9" -o macos-web-app-10.9.app/Contents/MacOS/Restar.arm64
